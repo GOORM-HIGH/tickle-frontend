bc9d9dda3708d44e3450c6c9b1b6381b93c83234
서해윤 <ggomulll3@gmail.com>
2025-08-11 11:32:08 +0900

    feat: 마이페이지 라우터 연결

---
+++ src/routes/mypageRouter.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MyPage from '../feat-mypage/pages/MyPage';
+import PointHistoryPage from '../feat-mypage/pages/PointHistoryPage';
+import PointChargePage from '../feat-mypage/pages/PointChargePage';
+
+const MypageRouter: React.FC = () => {
+  return (
+    <Routes>
+      {/* 마이페이지 메인 */}
+      <Route path="/" element={<MyPage />} />
+      
+      {/* 포인트 관련 라우트 */}
+      <Route path="/point/history" element={<PointHistoryPage />} />
+      <Route path="/point-charge" element={<PointChargePage />} />
+    </Routes>
+  );
+};
+
+export default MypageRouter;
