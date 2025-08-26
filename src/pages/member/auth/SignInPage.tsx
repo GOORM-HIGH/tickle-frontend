4760db5596701acb9bcbb0de7db2794daa85e0e9
Pangtaek <zombie3632@gmail.com>
2025-08-26 17:10:51 +0900

    chore: 불필요 주석 제거

--- src/pages/member/auth/SignInPage.tsx
+++ src/pages/member/auth/SignInPage.tsx
@@ -59,12 +59,10 @@ const SignInPage: React.FC = () => {
       const { accessToken, user } = response.data;
       setAccessToken(accessToken);
 
-      // 🎯 사용자 정보도 localStorage에 저장
       if (user) {
         localStorage.setItem("userInfo", JSON.stringify(user));
       }
 
-      // 🎯 페이지 새로고침으로 useAuth 상태 업데이트
       window.location.href = "/performance";
     } catch (error: any) {
       let errorMessage: string = "";
