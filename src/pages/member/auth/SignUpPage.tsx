829d9b02f10ebb6ad69392781db6d971950e70a4
Pangtaek <103546300+Pangtaek@users.noreply.github.com>
2025-09-17 16:12:02 +0900

    hotFix: 이메일 인증 코드 인증 실패 오류 수정

--- src/pages/member/auth/SignUpPage.tsx
+++ src/pages/member/auth/SignUpPage.tsx
@@ -126,7 +126,7 @@ const SignUpPage: React.FC = () => {
       }
 
       await axios.post(
-        "http://127.0.0.1:8081/api/v1/auth/email-verification/confirm",
+        "http://tickle.kr/api/v1/auth/email-verification/confirm",
         { email: formData.email, code: emailAuthCode },
         { headers: { "Content-Type": "application/json" } }
       );
