c807e6fe52dbe7c2159dfed0bd0417b37e13c542
Pangtaek <zombie3632@gmail.com>
2025-09-17 16:48:28 +0900

    hotFix: URL 오타 수정

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken } from "../utils/tokenUtils";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://tickle/kr",
+  baseURL: import.meta.env.VITE_API_BASE_URL ?? "https://api.tickle/kr",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
