549af2b7060e3d0080b44a1bdad67e2774a5052b
Pangtaek <zombie3632@gmail.com>
2025-09-17 16:39:32 +0900

    hotFix: 사업자 회원가입 실패 오류 수정

--- src/pages/member/auth/HostSignUpPage.tsx
+++ src/pages/member/auth/HostSignUpPage.tsx
@@ -158,7 +158,7 @@ const HostSignUpPage: React.FC = () => {
       }
 
       await axios.post(
-        "http://127.0.0.1:8081/api/v1/auth/email-verification",
+        "https://api.tickle/api/v1/auth/email-verification",
         { email: formData.email },
         { headers: { "Content-Type": "application/json" } }
       );
@@ -187,7 +187,7 @@ const HostSignUpPage: React.FC = () => {
         return;
       }
       await axios.post(
-        "http://127.0.0.1:8081/api/v1/auth/email-verification/confirm",
+        "https://api.tickle.kr/api/v1/auth/email-verification/confirm",
         { email: formData.email, code: emailAuthCode },
         { headers: { "Content-Type": "application/json" } }
       );
@@ -205,7 +205,7 @@ const HostSignUpPage: React.FC = () => {
     imageData.append("file", profileImage);
     try {
       const response = await axios.post(
-        "http://127.0.0.1:8081/api/v1/upload",
+        "https://api.tickle.kr/api/v1/upload",
         imageData,
         {
           headers: { "Content-Type": "multipart/form-data" },
@@ -248,7 +248,7 @@ const HostSignUpPage: React.FC = () => {
       console.log("payload:", payload);
 
       const response = await axios.post(
-        "http://127.0.0.1:8081/api/v1/sign-up",
+        "https://api.tickle/api/v1/sign-up",
         payload,
         {
           headers: { "Content-Type": "application/json" },
